02603156d1f0b98770ed8ece616b9207bedf2afd
Nick Costigan <nick.costigan@gmail.com>
2018-07-11 12:01:50 -0400

    sonarqube test

---
+++ sonarQubeTest/src/main/java/com/revature/Driver.java
@@ -0,0 +1,17 @@
+package com.revature;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		int x = 0;
+		switch (x) {
+		  case 0:
+		    System.out.println("case 0");
+		    break;
+		  default:
+			System.out.println("default");
+		    break;
+		}
+	}
+
+}
